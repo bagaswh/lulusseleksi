768b49d5c601e9742aaacba4a058471ec66606a2
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2021-05-04 05:17:04 +0700

    capek

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 server.listen(port, onListening);
 
 function onListening() {
